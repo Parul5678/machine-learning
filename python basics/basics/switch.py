db04b48a35be823082180d93ba9d5b66588386e9
Parul Malhotra <parulmalhotra27aug@gmail.com>
2026-08-20 17:52:20 +0530

    Delete python basics directory

--- python basics/basics/switch.py
+++
@@ -1,9 +0,0 @@
-num = 1
-match num:
-  case 1:
-    print("one")
-  case 2:
-    print("two")
-  case _:
-    print("incorrect")
-
--- python basics/modules/modules.py
+++
@@ -1,8 +0,0 @@
-def add():
-  return "addition"
-
-import array
-print("in array", __name__) #speical variable
-
-if __name__ == "__main__" #if thi is main file then only execute the file
-print(add())
